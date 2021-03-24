9e87e3a197162ac224f04b23f84f5bfecacf0e33
mich1991 <wozniak.michal91@gmail.com>
2021-03-24 13:42:13 +0000

    init commit

--- src/app/pages/movies/movie-details/movie-details.component.ts
+++ src/app/pages/movies/movie-details/movie-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpService } from '../../../services/http.service';
 import { Movie } from '../../../models/movie';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-movie-details',
@@ -11,12 +13,20 @@ import { Movie } from '../../../models/movie';
 export class MovieDetailsComponent implements OnInit {
   movieDetails: Observable<Movie>;
 
-  constructor(private http: HttpService) {
+  constructor(
+    private http: HttpService,
+    private route: ActivatedRoute,
+    private router: Router) {
   }
 
   ngOnInit() {
+    this.movieDetails = this.route.paramMap.pipe(
+      switchMap((params:ParamMap) => this.http.getMovie(params.get('id'))
+      )
+    )
   }
 
   goToMovies() {
+    this.router.navigate(['/movies'])
   }
 }
